5356a4c6302d365e572efb4052bbf7c37061eebd
Ignacio <reganveluz@gmail.com>
2021-10-27 07:26:34 +0800

    Added features: Driver information addition *license plate *car model
    Snippet for garage information
    *marker information

---
+++ app/src/main/java/com/example/authenticationapp/DriverInterface/CustomInfoWindowAdapter.java
@@ -0,0 +1,55 @@
+package com.example.authenticationapp.DriverInterface;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.authenticationapp.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter{
+
+    private final View mWindow;
+    private Context mContext;
+
+    public CustomInfoWindowAdapter(Context mContext) {
+        this.mContext = mContext;
+        mWindow = LayoutInflater.from(mContext).inflate(R.layout.custom_info_window, null);
+    }
+
+    private void rendowWindowText(Marker marker, View view){
+
+        String title = marker.getTitle();
+        TextView tvTitle = (TextView) view.findViewById(R.id.title);
+
+        if(!title.equals("")){
+            tvTitle.setText(title);
+        }
+
+        String snippet = marker.getSnippet();
+        TextView tvSnippet = (TextView) view.findViewById(R.id.snippet);
+
+        if(!snippet.equals("")){
+            tvSnippet.setText(snippet);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View getInfoWindow(@NonNull Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+
+    @Nullable
+    @Override
+    public View getInfoContents(@NonNull Marker marker) {
+        rendowWindowText(marker, mWindow);
+        return mWindow;
+    }
+}
